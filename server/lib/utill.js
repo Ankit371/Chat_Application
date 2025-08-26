ada77505e6d5e3ab3c7165f613b28ea760a9941e
Ankit Sharma <ankit8982120711@gmail.com>
2025-08-26 13:23:46 +0530

    Delete server directory

--- server/lib/utill.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-// Function to Generate a token for a User 
-
-export const generateToken = (userId)=>{
-    const token = jwt.sign({userId},process.env.JWT_SECRET);
-    return token;
-}
